86d65d79dd8c7494ce662af3d431ec75dee8e533
nattaparosky <tap19002@byui.edu>
2021-03-12 00:24:58 -0300

    WEEK 7 FIX

--- lesson7/js/gallerylazyload.js
+++ lesson7/js/gallerylazyload.js
@@ -8,7 +8,7 @@ const loadImages = (image) => {
 
 const imgOptions = {
   threshold: 0.5,
-  rootMargin: "0px 0px -100px 0px"
+  rootMargin: "0px 0px -200px 0px"
 };
 
 if('IntersectionObserver' in window) {
--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,31 +1,29 @@
-const images = document.querySelectorAll("[data-src]");
+let imgsToLoad = document.querySelectorAll("img[data-src");
 
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if(!src){
-        return;
-    }
-    img.src = src;
-}
 
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0px 0px 300px 0px"
-
+    rootMargin: "0 0 50px 0"
 };
 
-const imgObserver = new IntersectionObserver((entries,
-    imgObserver) => {
-        entries.forEach(entry => {
-            if (!entry.isIntersection){
-                return;
-            } else {
-                preloadImage(entry.target);
-                imgObserver.unobserve(entry.target);
-            }
-        })
-    }, imgOptions);
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
 
-    images.forEach(image => {
-        imgObserver.observe(image);
-    })
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else{
+            loadImages(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    });
+}, loadImages);
+
+imgsToLoad.forEach((img) => {
+    imgObserver.observe(img);
+});
