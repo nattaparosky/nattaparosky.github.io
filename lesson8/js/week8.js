6eb84cdde37741c54f6f1b5b9adda0be1eb6cb37
nattaparosky <tap19002@byui.edu>
2021-03-17 17:36:53 -0300

    fix menu 8

--- lesson8/js/week8.js
+++ lesson8/js/week8.js
@@ -1,5 +1,5 @@
 function toggleMenu() {
-    document.getElementsByClassName("navbar")[0].classList.toggle("responsive");
+    document.getElementsByClassName("navbar")[0].classList.toggle("menu-id");
    }
  
 const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
